85b28b09571b2896c6eec245afddd35749da4d1e
Christopher Duggan <christopherduggan@gmail.com>
2019-03-23 12:32:53 -0400

    Initial commit. Added characterObject.js and template character sheet

---
+++ characterObject.js
@@ -0,0 +1,14 @@
+const protagonist = {
+	name: 'Mysterious Stranger',
+	experience: 0,
+	level: 1,
+	attributes: [
+	{strength: 1},
+	{dexterity: 1},
+	{stamina: 1},
+	{charisma: 1},
+	{perception: 1},
+	{intelligence: 1},
+	{wits: 1}]
+	inventory: []
+}
