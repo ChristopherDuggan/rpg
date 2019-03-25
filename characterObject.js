542d3716bcd709c0b53b0c901edb97ea5f72f2e5
Christopher Duggan <christopherduggan@gmail.com>
2019-03-24 20:40:55 -0400

    cleaned up character object, messed around with inventory

--- characterObject.js
+++ characterObject.js
@@ -1,45 +1,52 @@
-const protagonist = {
+const char = {
 	name: 'Mysterious Stranger',
-	experience: 0,
+	experience: 50,
 	level: 1,
-	attributes: [
-	{strength: 1},
-	{dexterity: 1},
-	{stamina: 1},
-	{charisma: 1},
-	{will: 1},
-	{perception: 1},
-	{intelligence: 1},
-	{wits: 1}],
-	abilities: [
-		{talents : [
-			{alertness: 0},
-			{athletics: 0},
-			{brawl: 0},
-			{dodge: 0},
-			{empathy: 0},
-			{expression: 0},
-			{intimidation: 0},
-			{legerdemain: 0},
-			{subterfuge: 0}
-		]},
-		{skills: [
-			{'animal ken': 0},
-			{archery: 0},
-			{trade: 0},
-			{craft: 0},
-			{melee: 0},
-			{stealth: 0},
-			{survival: 0}
-		]},
-		{knowledges: [
-			{investigation: 0},
-			{medicine: 0}
-		]}
-	],
-	inventory: []
+	attributes: {
+		strength: 1,
+		dexterity: 1,
+		stamina: 1,
+		charisma: 1,
+		will: 1,
+		perception: 1,
+		intelligence: 1,
+		wits: 1},
+	abilities: {
+		talents: {
+			alertness: 0,
+			athletics: 0,
+			brawl: 0,
+			dodge: 0,
+			empathy: 0,
+			expression: 0,
+			intimidation: 0,
+			legerdemain: 0,
+			subterfuge: 0
+		},
+		skills: {
+			'animal ken': 0,
+			archery: 0,
+			trade: 0,
+			craft: 0,
+			melee: 0,
+			stealth: 0,
+			survival: 0
+		},
+		knowledges: {
+			investigation: 0,
+			medicine: 0
+		},
+	},
+	inventory: {
+		gold: 0,
+		stick: {
+			damage: 1,
+			range: 0,
+		}
+	}
 }
 
+console.log(char.inventory.stick);
 
 // Eperience Point Costs
 
